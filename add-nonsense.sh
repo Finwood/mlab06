049c5b36dca45243e428806546f48cc3c942570f
Fin <finwood@posteo.de>
2015-01-01 16:30:41 +0100

    syntax

--- add-nonsense.sh
+++ add-nonsense.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-echo "Nonsense Line." >> sense && git add sense && git commit -m added nonsense line to sense file && git push
+echo "Nonsense Line." >> sense && git add sense && git commit -m 'added nonsense line to sense file' && git push
